393f69453317bfc4edc433998528e0fd126bf356
Paramjeet SIngh <106370637+sp7183@users.noreply.github.com>
2022-05-30 16:57:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ import { loadAssets } from "./public/chars/loadAssets.js";
 import { story } from "./public/scene/story.js";
 import { loading } from "./public/scene/loading.js";
 import { pvp } from "./public/scene/pvp.js";
-import { touchControl } from "./public/chars/touchControl.js";
 loading();
 loadAssets();
 menu.loadAsset();
@@ -16,20 +15,4 @@ playerSelectMenu();
 Game();
 pvp();
 story();
-//---
-// go("loadingScreen", { nextScene: "intro" });
 go("intro");
-// go("storyBoard", { chapterNum: 0, beforeBattle: false });
-// go("game", {
-//   p1: 1,
-//   p2: 2,
-//   r: 1,
-//   p1r: 1,
-//   p2r: 0,
-//   mode: "test",
-//   chapter: 0,
-//   stageNum: 3,
-// });
-// go("pvpScene", { p1: 0, p2: 1 });
-// touchControl({ player: null, stand: null, data: null });
-// test();
